6b3ef455955e3b6a0eaa761997ac487963047286
Clément MARCEL <clmtmarcel@gmail.com>
2020-09-22 11:32:33 +0200

    Update UserRepository.class.php
    function add newUser

--- Realisations/Code/Repository/UserRepository.class.php
+++ Realisations/Code/Repository/UserRepository.class.php
@@ -25,6 +25,7 @@ class UserRepository implements IUserProvider {
         {
             $User[]= new User($data);
         }   
+        $rs->closeCursor();
         return $User;
     }
     public function findOne(int $id):User
@@ -33,11 +34,19 @@ class UserRepository implements IUserProvider {
         $stt-> bindValue('id',$id,PDO::PARAM_INT);
         $stt->execute();
         $data = $stt->fetch(PDO::FETCH_ASSOC);
+        $stt->closeCursor();
         return new User($data);
     }
-    
-    public function newUser()
-    {
 
+    public function newUser(User $newUser)
+    {
+        $stt = $this->con->prepare('INSERT INTO user VALUES firstname=:firstname, lastname=:lastname, email=:email, username=:username, password=:password, birthday=:birthday');
+        $stt->bindValue('firstname',$newUser['firstname'],PDO::PARAM_STR);
+        $stt->bindValue('lastname',$newUser['lastname'],PDO::PARAM_STR);
+        $stt->bindValue('email',$newUser['email'],PDO::PARAM_STR);
+        $stt->bindValue('username',$newUser['username'],PDO::PARAM_STR);
+        $stt->bindValue('password',$newUser['password'],PDO::PARAM_STR); 
+        $stt->execute();
     }
+    
 }
