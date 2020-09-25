0952582d2a87985d00038771fb9c32a96be34e59
Clement MARCEL <clmtmarcel@gmail.com>
2020-09-25 10:12:17 +0200

    use

--- Realisations/Code/Views/Authentication/Connection.php
+++ Realisations/Code/Views/Authentication/Connection.php
@@ -20,6 +20,8 @@ if(!empty($_POST['username']) && !empty($_POST['password'])) {
         $id_user = $authen->Compare($_POST['username'],$_POST['password']);
 
         if ($id_user > 0){
+            session_start();
+            $_SESSION['id'] = $id_user; 
             $UserRepo  = new UserRepository(Database::get()); 
             $user = $UserRepo->findOne($id_user);
             echo 'Bienvenue ' . $user->getUsername();
