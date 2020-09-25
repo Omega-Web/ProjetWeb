4b617a54a59140b258cb0971865155e954da3746
Reatir <51374309+Reatir@users.noreply.github.com>
2020-09-25 12:46:38 +0200

    Create Movie_userService.class.php

---
+++ Realisations/Code/Service/Movie_userService.class.php
@@ -0,0 +1,39 @@
+<?php
+namespace Code\Service;
+
+use Code\Model\Movie_user;
+use Code\Provider\IMovie_userProvider;
+use Code\Provider\IMovieProvider;
+
+class Movie_userService implements IMovie_userProvider
+{
+
+    private $movie_user;
+    private $movie;
+
+    public function __construct(IMovie_userProvider $mu,IMovieProvider $m)
+    {
+        $this->movie_user = $mu;
+        $this->movie=$m;
+    }
+
+    public function findAll(int $id_user): array
+    {
+        $movies_user = $this->movie_user->findAll($id_user);
+        foreach ($movies_user as $movie_user) {
+            $movie = $this->movie->findOne($movie_user->getId_movie());
+            $movie_user->setMovie($movie);
+        }
+        return $movies_user;
+    }
+
+    public function findOne(int $id_user, int $id_movie): Movie_user
+    {
+        $movie_user = $this->movie_user->findOne($id_user,$id_movie);
+        $movie = $this->movie->findOne($movie_user->getId_movie());
+        $movie_user->setMovie($movie);
+        return $movie_user;
+    }
+}
+
+?>
