ae8e75469bdef7dd146642da657bb026a7f5a046
Reatir <bastien.fernandes.3g3@gmail.com>
2020-10-04 00:16:42 +0200

    changement

--- Realisations/Code/Provider/IMovie_imageProvider.class.php
+++ Realisations/Code/Provider/IMovie_imageProvider.class.php
@@ -2,9 +2,15 @@
 
 namespace Code\Provider;
 
+Use Code\Model\Movie_image;
 
 interface IMovie_imageProvider {
-    public function findAll(int $id_movie): array;
 
-    public function findOne(int $id_movie,int $id_image): string;
+    public function findAll(): array;
+
+    public function findOne(int $id_image): Movie_image;
+
+    public function findAllByIdMovie(int $id_movie):array;
+
+
 }
--- Realisations/Code/Repository/Movie_imageRepository.class.php
+++ Realisations/Code/Repository/Movie_imageRepository.class.php
@@ -4,6 +4,7 @@ namespace Code\Repository;
 use PDO;
 use PDOException;
 use Code\Provider\IMovie_imageProvider;
+use Code\Model\Movie_image;
 
 class Movie_imageRepository implements IMovie_imageProvider
 {
@@ -12,10 +13,9 @@ class Movie_imageRepository implements IMovie_imageProvider
     {
         $this->con=$con;
     }
-    public function findAll(int $id_movie): array
+    public function findAll(): array
     {
-        $stt = $this->con->prepare('SELECT * FROM movie_image where fk_movie=:id_user');
-        $stt-> bindValue('id_user',$id_movie,PDO::PARAM_INT);
+        $stt = $this->con->prepare('SELECT * FROM movie_image');
         $stt->execute();
         while($data = $stt->fetch(PDO::FETCH_ASSOC))
         {
@@ -24,14 +24,25 @@ class Movie_imageRepository implements IMovie_imageProvider
         return $images;        
     }
 
-    public function findOne(int $id_movie, int $id_image): string
+    public function findOne(int $id_image): Movie_image
     {
-        $stt = $this->con->prepare('SELECT * FROM movie_image where fk_movie=:id_user and id=:id_image');
-        $stt-> bindValue('id_user',$id_movie,PDO::PARAM_INT);
+        $stt = $this->con->prepare('SELECT * FROM movie_image where id=:id_image');
         $stt-> bindValue('id_image',$id_image,PDO::PARAM_INT);
         $stt->execute();
-        $image = $stt->fetch(PDO::FETCH_ASSOC);    
-        return $image['image'];   
+        $data = $stt->fetch(PDO::FETCH_ASSOC);    
+        return new Movie_image($data);   
+    }
+
+    public function findAllByIdMovie(int $id_movie): array
+    {
+        $stt = $this->con->prepare('SELECT * FROM movie_image where fk_movie=:id_movie');
+        $stt-> bindValue('id_movie',$id_movie,PDO::PARAM_INT);
+        $stt->execute();
+        while($data = $stt->fetch(PDO::FETCH_ASSOC))
+        {
+            $images[]= $data;
+        }   
+        return $images; 
     }
 }
 ?>
