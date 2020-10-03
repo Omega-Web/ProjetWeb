21139781317a78f09cad3af81cb0e0cb552cb2ad
Gabe <desseresusinigabriel@gmail.com>
2020-10-03 23:00:18 +0200

    revert

--- Realisations/Code/Infrastructure/Database.class.php
+++ Realisations/Code/Infrastructure/Database.class.php
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
+
 namespace Code\Infrastructure;
 
 use PDO;
@@ -9,26 +7,27 @@ use PDOException;
 
 
 //classe static permettant de recuperer une connecion avec la base de données
-class Database {
+class Database
+{
 
     private static $con;
 
-    private function __contruct() {
-
+    private function __contruct()
+    {
     }
 
-    public static function get(){
+    public static function get()
+    {
 
         //si la connection n'existe pas la creer sinon la renvoyer
-        if (!self::$con){
+        if (!self::$con) {
             try {
-                
+
                 // self::$con = new PDO('mysql:host=localhost:8889,dbname=videotheque', 'admin_videotheque', 'admin_videotheque');
                 self::$con = new PDO('mysql:host=localhost;dbname=videotheque', 'root', 'root');
                 self::$con->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
                 self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$con->exec('SET NAMES "utf8"');
-                
             } catch (PDOException $e) {
 
                 //TODO mettre en systeme de log
@@ -37,54 +36,4 @@ class Database {
         }
         return self::$con;
     }
-
-=======
-=======
->>>>>>> parent of 95d5c1c... resolve conflit
-=======
->>>>>>> parent of 95d5c1c... resolve conflit
-<?php
-namespace Code\Infrastructure;
-
-use PDO;
-use PDOException;
-
-
-//classe static permettant de recuperer une connecion avec la base de données
-class Database {
-
-    private static $con;
-
-    private function __contruct() {
-
-    }
-
-    public static function get(){
-
-        //si la connection n'existe pas la creer sinon la renvoyer
-        if (!self::$con){
-            try {
-                
-                // self::$con = new PDO('mysql:host=localhost:8889,dbname=videotheque', 'admin_videotheque', 'admin_videotheque');
-                self::$con = new PDO('mysql:host=localhost;port=3307;dbname=videotheque', 'root', 'root');
-                self::$con->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
-                self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                self::$con->exec('SET NAMES "utf8"');
-                
-            } catch (PDOException $e) {
-
-                //TODO mettre en systeme de log
-                die($e->getMessage());
-            }
-        }
-        return self::$con;
-    }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 95d5c1c... resolve conflit
-=======
->>>>>>> parent of 95d5c1c... resolve conflit
-=======
->>>>>>> parent of 95d5c1c... resolve conflit
 }
