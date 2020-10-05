c877a7c73c54dfbdd67f07f77b811718f95f90a9
lecramc <clmtmarcel@gmail.com>
2020-10-05 19:38:58 +0200

    Sub Controller and sub views OK

--- Realisations/Code/Views/Authentication/Subscription.php
+++ Realisations/Code/Views/Authentication/Subscription.php
@@ -6,12 +6,9 @@ session_start();
 $_SESSION['id'];
 require_once '../../../bootstrap.php';
 
-use Code\Model\User;
-use Code\Repository\UserRepository;
-use Code\Infrastructure\Database;
+use Code\Controller\SubscriptionController;
 
-use PDOException;
-use PDO;
+$subController = new SubscriptionController();
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
@@ -29,9 +26,9 @@ if (
     $tabUser['password']    = $_POST['password'];
     $tabUser['email']       = $_POST['email'];
     $tabUser['birthday']    = $_POST['dob'];
+    $tabUser['fk_user_type']= 2; //Est un utilisateur
 
-    $UserRepo = new UserRepository(Database::get());
-    $UserRepo->createUser(new User($tabUser));
+    $subController->createUser($tabUser);
 } else {
 ?>
     <!DOCTYPE html>
