169e4fe9a58d135bc668a14ce2d128121efef783
Gabe <desseresusinigabriel@gmail.com>
2020-10-09 19:41:14 +0200

    update movieSearch moviesearch input field

--- Realisations/Code/Controller/MovieSearchController.class.php
+++ Realisations/Code/Controller/MovieSearchController.class.php
@@ -23,7 +23,7 @@ class MovieSearchController
         $movieStaffRepo = new Movie_staffRepository(Database::get());
         $staffRepo = new StaffRepository(Database::get());
 
-        $this->MovieService = new MovieService($movieRepo,$genreRepo,$movieImageRepo, $movieStaffRepo, $staffRepo);
+        $this->MovieService = new MovieService($movieRepo, $genreRepo, $movieImageRepo, $movieStaffRepo, $staffRepo);
     }
 
     public function getMovies()
@@ -32,7 +32,7 @@ class MovieSearchController
         return count($this->moviesArray);
     }
 
-    public function getTitle($index):string 
+    public function getTitle($index): string
     {
         return $this->moviesArray[$index]->getTitle();
     }
@@ -40,7 +40,7 @@ class MovieSearchController
     {
         return base64_encode($this->moviesArray[$index]->getImages()[0]['image']);
     }
-    public function getId($index):int
+    public function getId($index): int
     {
         return $this->moviesArray[$index]->getId();
     }
@@ -56,53 +56,40 @@ class MovieSearchController
         return count($this->moviesArray);
     }
 
-    public function getHtmlCard($text):string
+    public function getHtmlCard($text): string
     {
         $taille = $this->getMovieByTitle($text);
-        $html ="";
-        for ($i=0; $i < $taille; $i++) { 
-         
-        $html .= '<div class="card">';
-        $html .=    '<div class="div-img">';
-        $html .=        '<img id="card-img" src="data:image/jpeg;base64,' . $this->getImageBase64($i) . '" alt="imageMovie" />';
-        $html .=    "</div>";
-        $html .=    '<div class="container">';
-        $html .=        '<h4 class="title"><b>' . $this->getTitle($i) .' </b></h4>';
-        $html .=        ' <p>'. $this->getPlot($i) .'</p>';
-        $html .=        '<form action="../MovieInfo/MovieInfo.php" method="post">';
-        $html .=            ' <input type="text" name="movie-selected" value="' . $this->getId($i) .'" hidden />';
-        $html .=            '<button type="submit" id="seemore-btn">Plus</button>';
-        $html .=        '</form>';
-        $html .=    '</div>';
-        $html .= '</div>';
+        $html = "";
+        for ($i = 0; $i < $taille; $i++) {
+
+            $html .= '<div class="card">
+            <div class="div-img">
+                <img id="card-img" src="' . $this->getImageBase64($i) . ' alt="imageMovie" />
+            </div>
+            <div class="container">
+                <h4 class="title"><b>' . $this->getTitle($i) . '</b></h4>
+                <p>' . $this->getPlot($i) . '</p>
+                <form action="../MovieInfo/MovieInfo.php" method="post">
+                    <input type="text" name="movie-selected" value="' . $this->getId($i) . '" hidden />
+                    <button type="submit" id="seemore-btn">Plus</button>
+                </form>
+            </div>
+        </div>';
         }
-        
-        
-        
-           /* <h4 class="title"><b><?= $controller->getTitle($i) ?></b></h4>
-            <p><?= $controller->getPlot($i) ?></p>
-            <form action="../MovieInfo/MovieInfo.php" method="post">
-                <input type="text" name="movie-selected" value="<?= $controller->getId($i) ?>" hidden />
-                <button type="submit" id="seemore-btn">Plus</button>
-            </form>
-        </div>
-    </div>*/
         return $html;
     }
-    public function CallAjax($action,$text = "")
+    public function CallAjax($action, $text = "")
     {
-        switch($action)
-        {
+        switch ($action) {
             case 'search_movie':
-                $fp = fopen("log.txt","w");
-                fwrite($fp,"dans case \n");
+                $fp = fopen("log.txt", "w");
+                fwrite($fp, "dans case \n");
                 $html = $this->getHtmlCard($text);
-                fwrite($fp,$html);
+                fwrite($fp, $html);
                 fclose($fp);
                 $response = ['text' => "ok", 'html' => $html];
                 echo json_encode($response);
-            break;
-
+                break;
         }
     }
 }
