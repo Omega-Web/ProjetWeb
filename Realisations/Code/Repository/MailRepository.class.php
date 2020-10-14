132c4b6630ce59a07d773cfcbdf2744a62af0e8f
lecramc <clmtmarcel@gmail.com>
2020-10-14 23:25:20 +0200

    mail ok

--- Realisations/Code/Repository/MailRepository.class.php
+++ Realisations/Code/Repository/MailRepository.class.php
@@ -2,7 +2,7 @@
 namespace Code\Repository;
 class MailRepository
 {
-    public static function sendMail($email, $subject, $message, $headers)
+    public static function sendMail($email, $subject, $message, $headers):bool
     {
         return mail($email,$subject,$message,$headers);
     }
